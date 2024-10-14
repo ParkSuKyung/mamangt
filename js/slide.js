635cd09384ba37e304613880aedf18e951e8def9
ParkSuKyung <jhsk0823@naver.com>
2024-10-14 13:55:30 +0900

    마망갸또
    마망갸또 메인 반응형 완료

---
+++ js/slide.js
@@ -0,0 +1,34 @@
+
+        // $(document).ready(function(){
+
+        //     var swiper = new Swiper(".mySwiper", {
+        //     pagination: {
+        //         el: ".swiper-pagination",
+        //         type: "progressbar",
+        //     },
+        //     navigation: {
+        //         nextEl: ".swiper-button-next",
+        //         prevEl: ".swiper-button-prev",
+        //     },
+        //     });
+
+        // });
+
+        $(document).ready(function(){
+        var swiper = new Swiper(".mySwiper", {
+            slidesPerView: 1,
+            // spaceBetween: 20,
+            pagination: {
+              el: ".swiper-pagination",
+              clickable: true,
+            },
+          });
+
+        });
+
+        
+
+
+
+        
+
